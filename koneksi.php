0fecbb69a9dfefdd92ad8a8b6d3112838c99e378
taroandmatcha <151890694+taroandmatcha@users.noreply.github.com>
2024-01-04 15:54:20 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -9,7 +9,8 @@
 			try {
 				$dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 				$dbConnection->exec("set names utf8");
-
+                		$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                		return  $dbConnection;
 			}
 			catch (PDOException $e) {
 				return 'Connection failed: ' . $e->getMessage();
